4feefcf82de973a6587225df3f304c5432d55819
Alvin Zhu <alvinzhu33@yahoo.com>
2016-09-16 15:35:26 -0400

    Euler #2

---
+++ Euler_9.16/euler2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+  int store=1;
+  int make=2;
+  int counter=0;
+  while(store + make < 4000000){
+    if(make%2 == 0){
+      counter+=make;
+    }
+    
+    
+  }
+}
